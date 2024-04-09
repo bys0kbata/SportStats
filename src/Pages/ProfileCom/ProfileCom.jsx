49bc04418383ddd1d666ff4c9e01df3639200ace
bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2024-04-09 17:49:43 +0400

    Update ProfileCom.jsx

--- src/Pages/ProfileCom/ProfileCom.jsx
+++ src/Pages/ProfileCom/ProfileCom.jsx
@@ -7,6 +7,7 @@ export default function ProfileComm(){
     const href = window.location.href;
     const idCom= href.match(/\d+$/);
     const [data, setData]= useState([]);
+    const [squad, setSquad]= useState([]);
     const nav = useNavigate();
     const config =   {headers: {
         'X-Auth-Token':'1fe2051c3808448ca25c9cae41e88191'  
@@ -16,19 +17,37 @@ export default function ProfileComm(){
         .then((req)=>{
             setData(req.data);
             console.log(data);
+            setSquad(req.data.squad);
         })
     },[])
     return(
         <div className="ProfilePlayer">
             <div className="ProfileCom">
-                <img  />
-                <h4>Название: {data.name}</h4>
+            <img src={data.crest}  />
+                <div><h4>Название: {data.name}</h4>
                 <h4>Адрес: {data.address}</h4>
-                <h4>Тренер: {data}</h4>
+                <h4>Тренер: {data.coach?.name}</h4>
                 <h4>Веб-сайт: {data.website}</h4>
-                <h4>Цвета команды: {data.clubcolor}</h4>
+                <h4>Цвета команды: {data.clubColors}</h4></div>
             </div>
-
+            <table>
+                <thead>Игроки команды</thead>
+                <tr>
+                    <th> ФИО</th>
+                    <th> Позиция</th>
+                    <th></th>
+                </tr>
+                    {squad.map((one)=>{
+                        return(
+                            <tr key={squad.id}>
+                                <td>{one.name}</td>
+                                <td>{one.position}</td>
+                                <td><button onClick={()=>{nav(`/profileplayer/${one.id}`);}}>👁️</button></td>
+                            </tr>
+                        )
+                    })}
+                    
+            </table>
         </div>
     )
 
